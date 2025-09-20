d274f2c21ef96251ce073e1ea065a7ccc0d42365
Anubhav-Srivastav-18 <srivastavanubhav018@gmail.com>
2025-09-20 14:11:05 +0530

    break case.c.c

--- break case.c.c
+++ break case.c.c
@@ -1,26 +1,15 @@
 #include <stdio.h>
-int main ()
- 
- {
- 	int number;
- 	
- 	printf("Enter the number :");
- 	scanf("%d", &number);
- 	
- 	if (number<100 & number>40)
- 	{
- 		printf("student has passed");
-	 }
- 	else if (number<40 & number>33)
- 	{
- 		printf("the student has barely passed");
- 		
-	 }
-	 else 
-	 {
-	 	printf("the student is failed");
-	 }
-	 
-	 
- 	return 0;
- }
+
+int main() {
+    int i = 1;
+    while (i <= 10) {
+        if (i == 7) {
+            printf("Loop stopped at i = %d\n", i);
+            break;   // Exit loop when i == 7
+        }
+        printf("i = %d\n", i);
+        i++;
+    }
+    return 0;
+}
+
